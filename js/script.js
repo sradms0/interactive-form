60834609d741f3892d4a810f8b3e0a7ee4084f15
sradms0 <sradms0@openmailbox.org>
2019-06-16 17:29:16 -0400

    add listener for shirt changes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
+const $nameInput = $('#name');
 const $otherJobInput = $('#other-title');
 const $jobRoleSelect = $('#title');
 
+
+const $designSelect = $('#design');
+const $colorsDiv = $('#colors-js-puns');
+const $colorSelect = $('#color');
+
+// toggle other rob role text field
 $jobRoleSelect.on('change', function() {
   if (this.value === 'other') {
     $otherJobInput.show();
@@ -9,8 +16,28 @@ $jobRoleSelect.on('change', function() {
   }
 });
 
+// display and hide shirt colors based on shirt design
+$designSelect.on('change', function() {
+  const $heartJSOptions =  $('option:contains("I ♥ JS shirt only")');
+  const $JSPunOptions = $('option:contains("JS Puns shirt only")');
+
+  $colorsDiv.show();
+
+  if (this.value === 'heart js') {
+    $heartJSOptions.show().eq(0).prop('selected', true);
+    $JSPunOptions.hide();
+  } else if (this.value === 'js puns') {
+    $JSPunOptions.show().eq(0).prop('selected', true);;
+    $heartJSOptions.hide();
+  } else {
+    $colorsDiv.hide();
+  } 
+});
+
+
+
 // set focus on first text field
-$('#name').focus();
+$nameInput.focus();
 
 // hide other job role text field
 $otherJobInput.hide();
