d5cdaa6f574cc3c2cc5888f7f441e7c01c7961c4
sradms0 <sradms0@openmailbox.org>
2019-06-16 11:39:05 -0400

    save 'other' job input and toggle visibility with select change listener

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
+const $otherJobInput = $('#other-job');
+const $jobRoleSelect = $('#title');
+
+$jobRoleSelect.on('change', function() {
+  if (this.value === 'other') {
+    $otherJobInput.show();
+  } else {
+    $otherJobInput.hide();
+  }
+});
+
 // set focus on first text field
 $('#name').focus();
 
 // hide other job role text field
-$('#other-job').hide()
+$otherJobInput.hide();
