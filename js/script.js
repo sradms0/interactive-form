cedc91c25c1701c89149e92ca7d9a94451356102
sradms0 <sradms0@openmailbox.org>
2019-06-17 10:24:26 -0400

    change extractDate to extractString for enhanced extraction between date and price

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ const $nameInput = $('#name');
 const $otherJobInput = $('#other-title');
 const $jobRoleSelect = $('#title');
 
-
 /**** t-shirt info ****/
 const $designSelect = $('#design');
 const $colorsDiv = $('#colors-js-puns');
@@ -12,29 +11,37 @@ const $colorSelect = $('#color');
 /**** activities info ****/
 const $activitiesFieldset = $('.activities');
 
-
 /**** event listeners ****/
 // disable/enable activities based on schedule, and add/subtract total
 $activitiesFieldset.on('change', function(e) {
-  const extractDate = string => {
-    const regex = /\w+ \d+\w\w-\d+\w\w/i;
-    let date = string.match(regex);
-    if (date) date = date[0];
-    return date
+  const extractString = ($, { type }) => {
+    const string = $.text();
+    const dateRegex = /\w+ \d+\w\w-\d+\w\w/i;
+    const priceRegex = /\$\d+/;
+    let selectedRegex;
+    let extracted;
+
+    if (type === 'date') selectedRegex = dateRegex;
+    else if (type === 'price') selectedRegex = PriceRegex;
+
+    extracted = string.match(selectedRegex);
+    if (extracted) extracted = extracted[0];
+    return extracted;
   };
+
   const extractInput = $ => $.children(':first');
 
   // save selected activity and date, and other unselected activities  
   const $activityLabel = $(e.target.parentNode);
   const $activityInput = extractInput($activityLabel);
-  const date = extractDate($activityLabel.text());
+  const date = extractString($activityLabel, {type: 'date'});
 
   // search for any conflicting day and time
   const $otherActivityLabels = $(this).find('input:not(:checked)').parent();
   $otherActivityLabels.each(function(i) {
     const $nextActivityLabel = $(this);
     const $nextActivityInput = extractInput($nextActivityLabel);
-    const nextDate = extractDate($nextActivityLabel.text());
+    const nextDate = extractString($nextActivityLabel, {type: 'date'});
 
     // if dates are equal: enable or disable checkbox according to status
     if (date === nextDate) {
